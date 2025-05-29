eb04c5fd78501f490073f819a7e327340898732f
Aouadi Brahim <aouadib86@gmail.com>
2025-05-29 23:05:12 +0100

    first endpoint

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("test");
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello from the server' });
+});
+
+app.listen(process.env.PORT, () => {
+    console.log('Server is running...');
+});
