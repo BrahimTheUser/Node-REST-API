4f4bbfe18dcbacb4c365e0d31a1713ecf8c91142
Aouadi Brahim <aouadib86@gmail.com>
2025-05-29 23:20:42 +0100

    added some security middlewares, like cors ...

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const helmet = require('helmet'); // middleware that helps secure Express apps by setting various HTTP headers
+const cors = require('cors'); // middleware that enables cross-origin resource sharing (CORS)
+const cookieParser = require('cookie-parser'); // middleware that parses the cookie header and populate req.cookies with an object keyed by the cookie names.
 
 const app = express();
+app.use(cors());
+app.use(helmet());
 app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
     res.json({ message: 'Hello from the server' });
 });
