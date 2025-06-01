a70866a2108e3b2f069e807076921996ba9976a4
Aouadi Brahim <aouadib86@gmail.com>
2025-06-01 19:24:10 +0100

    added mongo database connection, and created user and post models

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const helmet = require('helmet'); // middleware that helps secure Express apps by setting various HTTP headers
 const cors = require('cors'); // middleware that enables cross-origin resource sharing (CORS)
 const cookieParser = require('cookie-parser'); // middleware that parses the cookie header and populate req.cookies with an object keyed by the cookie names.
+const mongoose = require('mongoose');
 
 const app = express();
 app.use(cors());
@@ -9,6 +10,15 @@ app.use(helmet());
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
+
+mongoose
+	.connect(process.env.MONGO_URI)
+	.then(() => {
+		console.log('Database connected');
+	})
+	.catch((err) => {
+		console.log(err);
+	});
 
 app.get('/', (req, res) => {
     res.json({ message: 'Hello from the server' });
